6df9d10f1a7c30206be1738ccf0f311f7034c5f7
Poul <poulnichols@outlook.com>
2020-05-06 17:41:48 -0700

    Updating Readme File with my name, today's date and note about completing assignment.  I also added the additional code for assignment 2.

--- README.md
+++ README.md
@@ -6,3 +6,6 @@ Course title: AD 340 - 5210
 Poul Nichols
 
 Poul Nichols 5/1/2020
+
+Poul Nichols 5/6/2020
+completed Assignment 2
--- app/src/main/java/com/example/ad340/MainActivity.kt
+++ app/src/main/java/com/example/ad340/MainActivity.kt
@@ -5,10 +5,13 @@ import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.lifecycle.Observer
+import androidx.recyclerview.widget.LinearLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
-
+    private val forecastRepository = ForecastRepository()
     // region setup methods
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -23,14 +26,27 @@ class MainActivity : AppCompatActivity() {
             if(zipcode.length != 5){
                 Toast.makeText(this, R.string.zipcode_entry_error, Toast.LENGTH_SHORT).show()
             }else {
-                Toast.makeText(this, zipcode, Toast.LENGTH_SHORT).show()
+                forecastRepository.loadForecast(zipcode)
             }
+        }
+        val forecastList: RecyclerView = findViewById(R.id.forecastList)
+        forecastList.layoutManager = LinearLayoutManager(this)
+        val dailyForecastAdapter = DailyForecastAdapter(){forecastItem ->
+            val msg = getString(R.string.forecast_click_format, forecastItem.temp, forecastItem.description)
+            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        }
+        forecastList.adapter = dailyForecastAdapter
+
+        val weeklyForecastObserver = Observer<List<DailyForecast>>{forecastItems ->
+            // update our list adapter
+            dailyForecastAdapter.submitList(forecastItems)
 
         }
-
-
+        forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver)
     }
 
+
+    /* Not using these lifecycle methods for this week (3rd week)
     override fun onStart() {
         super.onStart()
     }
@@ -54,6 +70,7 @@ class MainActivity : AppCompatActivity() {
     override fun onDestroy() {
         super.onDestroy()
     }
+    */
 
     // endregion teardown methods
 }
