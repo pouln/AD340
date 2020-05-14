13dd6f61aacec434af356c3053f77db7884ffff5
Poul <poulnichols@outlook.com>
2020-05-14 07:26:34 -0700

    Updated program for Assignment 4

--- README.md
+++ README.md
@@ -9,3 +9,6 @@ Poul Nichols 5/1/2020
 
 Poul Nichols 5/6/2020
 completed Assignment 3
+
+Poul Nichols 5/13/2020
+completed Assignment 4
--- app/src/main/java/com/example/ad340/MainActivity.kt
+++ app/src/main/java/com/example/ad340/MainActivity.kt
@@ -1,22 +1,31 @@
 package com.example.ad340
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuInflater
+import android.view.MenuItem
 import android.widget.Button
 import android.widget.EditText
 import android.widget.Toast
+import androidx.appcompat.app.AlertDialog
 import androidx.lifecycle.Observer
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.example.ad340.details.ForecastDetailsActivity
 import kotlinx.android.synthetic.main.activity_main.view.*
 
 class MainActivity : AppCompatActivity() {
     private val forecastRepository = ForecastRepository()
+    private lateinit var tempDisplaySettingManager: TempDisplaySettingManager
     // region setup methods
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        tempDisplaySettingManager = TempDisplaySettingManager(this)
 
         val zipcodeEditText: EditText = findViewById(R.id.zipcodeEditText)
         val enterButton: Button = findViewById(R.id.enterButton)
@@ -29,13 +38,12 @@ class MainActivity : AppCompatActivity() {
                 forecastRepository.loadForecast(zipcode)
             }
         }
-        val forecastList: RecyclerView = findViewById(R.id.forecastList)
-        forecastList.layoutManager = LinearLayoutManager(this)
-        val dailyForecastAdapter = DailyForecastAdapter(){forecastItem ->
-            val msg = getString(R.string.forecast_click_format, forecastItem.temp, forecastItem.description)
-            Toast.makeText(this, msg, Toast.LENGTH_SHORT).show()
+        val dailyForecastList: RecyclerView = findViewById(R.id.forecastList)
+        dailyForecastList.layoutManager = LinearLayoutManager(this)
+        val dailyForecastAdapter = DailyForecastAdapter(tempDisplaySettingManager){forecast ->
+            showForecastDetails(forecast)
         }
-        forecastList.adapter = dailyForecastAdapter
+        dailyForecastList.adapter = dailyForecastAdapter
 
         val weeklyForecastObserver = Observer<List<DailyForecast>>{forecastItems ->
             // update our list adapter
@@ -43,6 +51,32 @@ class MainActivity : AppCompatActivity() {
 
         }
         forecastRepository.weeklyForecast.observe(this, weeklyForecastObserver)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        val inflater: MenuInflater = menuInflater
+        inflater.inflate(R.menu.settings_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        // handle item selection
+        return when (item.itemId){
+            R.id.tempDisplaySetting -> {
+                //do something
+                showTempDisplaySettingDialog(this, tempDisplaySettingManager)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
+    }
+
+    private fun showForecastDetails(forecast: DailyForecast){
+        val forecastDetailsIntent = Intent(this, ForecastDetailsActivity::class.java)
+        forecastDetailsIntent.putExtra("key_temp", forecast.temp)
+        forecastDetailsIntent.putExtra("key_description", forecast.description)
+
+        startActivity(forecastDetailsIntent)
     }
 
 
